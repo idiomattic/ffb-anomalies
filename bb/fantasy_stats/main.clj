ecca20b6ce72f0b5de099af18122fce6f3054442
Matt Lese <matthewjlese@gmail.com>
2025-11-11 10:44:13 -0800

    basic project setup

---
+++ bb/fantasy_stats/main.clj
@@ -0,0 +1,5 @@
+(ns fantasy-stats.main
+  (:require [medley.core :as m]))
+
+(defn -main [& _args]
+  (prn (m/index-by :id [{:id 1} {:id 2}])))
---
+++ test/fantasy_stats/fantasy_stats_test.clj
@@ -0,0 +1,7 @@
+(ns fantasy-stats.fantasy-stats-test
+  (:require [clojure.test :refer :all]
+            [fantasy-stats.fantasy-stats :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

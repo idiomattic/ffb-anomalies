5076f913715b459d32f8f1450b072282ac79e61c
Matt Lese <matthewjlese@gmail.com>
2025-11-11 13:55:56 -0800

    adds basic grouping/parsing of data

--- src/fantasy_stats/fantasy_stats.clj
+++ src/fantasy_stats/fantasy_stats.clj
@@ -1,12 +1,37 @@
 (ns fantasy-stats.fantasy-stats
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [clojure.edn :as edn])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn read-data []
+  (let [all-files (.list (io/file "resources/"))]
+    (mapv #(edn/read-string (slurp (io/resource %))) all-files)))
+
+(defn calculate-points-against-streaks [season-data]
+  (let [{:keys [season roster-id->username matchups]} season-data]
+    ))
+
+(defn parse-matchups
+  "Takes a week's matchups, returning a sequence of maps containing :username, :points-for, and :points-against."
+  [matchups week roster-id->username]
+  (as-> matchups $
+    (group-by :matchup_id $)
+    (update-vals $ (fn [[roster-a roster-b]]
+                     [{:username (get roster-id->username (:roster_id roster-a))
+                       :week week
+                       :points-for (:points roster-a)
+                       :points-against (:points roster-b)}
+                      {:username (get roster-id->username (:roster_id roster-b))
+                       :week week
+                       :points-for (:points roster-b)
+                       :points-against (:points roster-a)}]))
+    (vals $)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (doseq [season-data (read-data)]
+    (let [{matchups-by-week :matchups roster-id->username :roster-id->username} season-data]
+      (doseq [week-data matchups-by-week]
+        (let [{:keys [season week matchups]} week-data]
+          (pp/pprint (parse-matchups matchups week roster-id->username)))))))
